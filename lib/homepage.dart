bb2d0908b2280eed672a9ad07781b9adfa577967
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2021-05-18 20:21:22 +0530

    added 2

---
+++ lib/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  int days = 2;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to day $days'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to flutter this is day  $days '),
+        ),
+      ),
+    );
+  }
+}
