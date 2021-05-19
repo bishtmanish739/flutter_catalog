dc0e13ab6e625313073ceac9b60189ef9b0103b2
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2021-05-19 17:57:03 +0530

    Create catalog.dart

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final product = [
+  Item(
+      id: "djasjfas",
+      name: "green screen",
+      desc: " ajsdfkajt fjdjsa ",
+      price: 1000,
+      color: "#000000",
+      image: "www.gmail.com ")
+];
