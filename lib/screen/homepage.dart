60da9dc9a66bf230bf1da7ab6868133d2059896e
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2021-05-19 21:39:17 +0530

    added json

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 import 'package:flutter_catalog/widgets/mydrawer.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
-  int days = 12;
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int days = 13;
+
   final dummylist = List.generate(50, (index) => CatalogModel.item[0]);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    //print(catalogJson);
+    var decodedata = jsonDecode(catalogJson);
+    //print(decodedata);
+    final productdata = decodedata['products'];
+    //print(productdata);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
